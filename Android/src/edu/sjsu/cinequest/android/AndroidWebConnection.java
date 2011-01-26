b29e4256add7de842783848df713d25a3d9fa67d
Cay Horstmann <cay@horstmann.com>
2011-01-26 17:19:19 +0700

    Tinkering with timeout

--- Android/src/edu/sjsu/cinequest/android/AndroidWebConnection.java
+++ Android/src/edu/sjsu/cinequest/android/AndroidWebConnection.java
@@ -15,6 +15,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
@@ -24,6 +25,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.HTTP;
@@ -43,6 +45,9 @@ public class AndroidWebConnection extends WebConnection {
     	HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
     	HttpProtocolParams.setContentCharset(params, HTTP.DEFAULT_CONTENT_CHARSET);
     	HttpProtocolParams.setUseExpectContinue(params, true);
+    	HttpConnectionParams.setConnectionTimeout(params, 1000);
+    	HttpConnectionParams.setSoTimeout(params, 1000);
+    	ConnManagerParams.setTimeout(params, 1000);
 
     	SchemeRegistry registry = new SchemeRegistry();
     	registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
