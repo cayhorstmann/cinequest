7539109baf65dbad78d4857881787023f082d861
SCK <SnigdhaChaitanya@Chaitanya-Kosarajus-MacBook-Pro.local>
2012-05-31 13:47:41 -0700

    Fixing issue 0000088 Html5 code modifications

--- Html5/scripts/headerbar.js
+++ Html5/scripts/headerbar.js
@@ -1,10 +1,14 @@
+/**
+ * author: Snigdha Mokkapati
+ */
+
 var str="<h1>Cinequest</h1>"+
     "<div data-role='controlgroup' data-type='horizontal' id='btn_group'>"+
-    "<a href='index.html' data-role='button' data-inline='true'>Home</a>"+
+    "<a href='index.html' data-role='button' data-inline='true' data-icon='home'>Home</a>"+
     "<a href='swipe_page.html' data-role='button' rel='external'"+
     "data-inline='true' data-transition='flip'>Swipe Mode</a>"+
     "</div>"+
-    "<a href='#' data-role='button' class='ui-btn-right'>Info</a>"+
+    "<a href='info.html' data-rel='dialog' class='ui-btn-right' data-icon='info' data-transition='pop'>Info</a>"+
     "<div data-role='navbar'>"+
     "<ul><li><a href='films.html' id='filmstag'>FILMS</a></li>"+
     "<li><a href='events.html'>EVENTS</a></li>"+
